6137b35a26b0b75e69a7d5a7f10b992d97837754
gregeva <evang0911@gmail.com>
2023-02-13 22:52:29 -0700

    Add project files.

---
+++ DateMe/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using DateMe.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateMe.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BigChillinContext _privateChillinContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, BigChillinContext someName)
+        {
+            _logger = logger;
+            _privateChillinContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Moves()
+        {
+            return View("movieForm");
+        }
+
+        [HttpPost]
+        public IActionResult Moves(MovieSubmission ms)
+        {
+            _privateChillinContext.Add(ms);
+            _privateChillinContext.SaveChanges();
+            return View("conformation", ms);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DateMe/Models/BigChillinContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateMe.Models
+{
+    public class BigChillinContext : DbContext
+    {
+        //constructor
+        public BigChillinContext(DbContextOptions<BigChillinContext> choices) : base (choices)
+        {
+            // leave blank for now
+        }
+
+        public DbSet<MovieSubmission> Submissions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieSubmission>().HasData(
+
+                    new MovieSubmission
+                    {
+                        MovieID = 1,
+                        Category = "Sci-Fi",
+                        Title = "Interstellar",
+                        Year = 2014,
+                        Director = "Christopher Nolan",
+                        Rating = "PG-13",
+                        Edited = false,
+                        LentTo = "",
+                        Notes = "Masterpiece"
+                    },
+                    new MovieSubmission
+                    {
+                        MovieID = 2,
+                        Category = "Romance/Satire",
+                        Title = "The Graduate",
+                        Year = 1967,
+                        Director = "Mike Nichols",
+                        Rating = "PG",
+                        Edited = false,
+                        LentTo = "",
+                        Notes = "Hello Darkness"
+                    },
+                    new MovieSubmission
+                    {
+                        MovieID = 3,
+                        Category = "Adventure",
+                        Title = "Puss in Boots: The Last Wish",
+                        Year = 2022,
+                        Director = "Joel Crawford",
+                        Rating = "PG",
+                        Edited = false,
+                        LentTo = "",
+                        Notes = "The most epic tale we've seen since Homer's 'Odyssey'"
+                    }
+                );
+        }
+    }
+}
